c748fb09fad43dfb7603f80166bd01c20a781ca2
Dvlv <Dvlv@users.noreply.github.com>
2022-07-09 12:42:07 +0100

    whitespace

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, Gio, GLib, GObject
 
 
-
-
-
 if __name__ == "__main__":
     from main_window import MyWindow
+
     win = MyWindow()
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
